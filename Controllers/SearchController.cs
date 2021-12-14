8e4fa75f1ece40420114308d2286284ab0946231
sugan <sugan@LAPTOP-9RONT2IT>
2021-12-13 20:56:05 -0500

    Add project files.

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using SearchRecord.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchRecord.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            Decthirteen_OneEntities dbContext = new Decthirteen_OneEntities();
+            return View(dbContext.Employees.ToList());
+           // return View();
+        }
+        [HttpPost]
+        public ActionResult Index(DateTime From, DateTime To)
+        {
+            Decthirteen_OneEntities dbContext = new Decthirteen_OneEntities();
+            return View(dbContext.Usp_Empsearch(From, To));
+
+
+
+        }
+    }
+}
